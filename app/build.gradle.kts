8ab34fbecb6e7b558b3e31023a1a5d54ecd28b55
DaneMakes <dane.barland@gmail.com>
2026-09-06 16:16:42 -0700

    Fix localized LATERAL_ app label

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,8 +55,8 @@ android {
     defaultConfig {
         minSdk = 26
         targetSdk = 37
-        versionCode = 3
-        versionName = "0.2.1"
+        versionCode = 4
+        versionName = "0.2.2"
         buildConfigField("boolean", "VITURE_SDK_ENABLED", vitureSdkEnabled.toString())
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
